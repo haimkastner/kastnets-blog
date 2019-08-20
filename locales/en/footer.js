241dfd4fc8b3f3f2037bd9d7807f37e3239b6cad
haim kastner <haim.kastner@gmail.com>
2019-08-20 23:43:40 +0300

    Add RSS link to the footer

--- locales/en/footer.js
+++ locales/en/footer.js
@@ -2,6 +2,7 @@ const user = '<a target="_blank" href="mailto: haim.kastner@gmail.com" class="an
 const main = 'Software Developer - Coding and Development Thoughts Blog, The Content is on the responsibility of the mind reader.';
 const git = '<a target="_blank" href="https://github.com/haimkastner" class="ani"><span>GitHub</span></a>';
 const twitter = '<a target="_blank" href="https://twitter.com/hkastnet" class="ani"><span>Twitter</span></a>';
+const rss = '<a target="_blank" href="https://blog.castnet.club/en/feed.xml" class="ani"><span>RSS</span></a>';
 export default {
-  credits: `${user} - ${main} also at ${git} and at ${twitter}.`,
+  credits: `${user} - ${main} | ${git} | ${twitter} | ${rss} |`,
 }
