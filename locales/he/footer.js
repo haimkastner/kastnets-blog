eb303414da49d3c1e410ceed48a678b13e561419
haim kastner <haim.kastner@gmail.com>
2019-08-23 02:03:05 +0300

    set rss feed file relative to the domain link articals to the master branch on repo.

--- locales/he/footer.js
+++ locales/he/footer.js
@@ -2,7 +2,7 @@ const user = '<a target="_blank" href="mailto: haim.kastner@gmail.com" class="an
 const main = 'מפתח תכנה - בלוג מחשבות על קידוד ופיתוח, התוכן באחריות קורא המחשבות.';
 const git = '<a target="_blank" href="https://github.com/haimkastner" class="ani"><span>GitHub</span></a>';
 const twitter = '<a target="_blank" href="https://twitter.com/hkastnet" class="ani"><span>Twitter</span></a>';
-const rss = '<a target="_blank" href="https://blog.castnet.club/feed" class="ani"><span>RSS</span></a>';
+const rss = '<a target="_blank" href="/feed" class="ani"><span>RSS</span></a>';
 export default {
   credits: `${user} - ${main} | ${git} | ${twitter} | ${rss} |`,
 }
