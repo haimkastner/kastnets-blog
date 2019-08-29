d2398c0b57267bbd7638b058c53c004ad7f386b3
haim kastner <haim.kastner@gmail.com>
2019-08-30 00:15:38 +0300

    Update blog footer - Center the text. - Move links to new line. - Add template credit.

--- components/Sections/Footer.vue
+++ components/Sections/Footer.vue
@@ -3,11 +3,39 @@
     <div class="footer__info">
       <div class="container footer__info-container">
         <div class="footer__copyright">
-          © {{ year }} — <span v-html="$t('footer.credits')"/>
+          © {{ year }} —
+          <span v-html="$t('footer.credits')" />
+          <br />
+          <br />
+          <span>
+            |
+            <a target="_blank" href="https://github.com/haimkastner" class="ani">
+              <span>GitHub</span>
+            </a>
+            |
+            <a target="_blank" href="https://twitter.com/hkastnet" class="ani">
+              <span>Twitter</span>
+            </a>
+            |
+            <a target="_blank" href="/en/feed" class="ani">
+              <span>RSS</span>
+            </a>
+            |
+          </span>
+          <br />
+          <br />
+          <span>
+            Blog template based on the Marina Aisa
+            <a
+              target="_blank"
+              href="https://github.com/marinaaisa/nuxt-markdown-blog-starter"
+              class="ani"
+            >nuxt blog starter</a>
+          </span>
         </div>
         <!-- <a href="#top" class="footer__top-page">
           ^
-        </a> -->
+        </a>-->
       </div>
     </div>
   </footer>
@@ -15,51 +43,49 @@
 <script>
 export default {
   computed: {
-    year () {
-      return new Date().getFullYear()
+    year() {
+      return new Date().getFullYear();
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
 .footer {
-	&__info {
-		background-color: $background-secondary;
-		padding: 1.5em 0;
+  &__info {
+    background-color: $background-secondary;
+    padding: 1.5em 0;
   }
-  
+
   &__info-container {
     display: flex;
     justify-content: space-between;
     width: 100%;
     flex-direction: column;
 
-    @media (min-width: $screen-sm){
+    @media (min-width: $screen-sm) {
       flex-direction: row;
     }
   }
 
-	&__copyright {
-		text-align: center;
-		@media (min-width: $screen-sm) {
-			text-align: inherit;
-		}
-	}
+  &__copyright {
+    text-align: center;
+    margin: auto;
+  }
 
-	&__top-page {
-		color: $text-negative;
-		display: block;
-		text-align: center;
-		line-height: 1;
-		-webkit-transition: 1s;
-		-moz-transition: 1s;
+  &__top-page {
+    color: $text-negative;
+    display: block;
+    text-align: center;
+    line-height: 1;
+    -webkit-transition: 1s;
+    -moz-transition: 1s;
     transition: 1s;
-    
-		@media (min-width: $screen-sm){
-			text-align: right;
-		}
-	}
+
+    @media (min-width: $screen-sm) {
+      text-align: right;
+    }
+  }
 }
 </style>
 
--- locales/en/footer.js
+++ locales/en/footer.js
@@ -1,8 +1,5 @@
 const user = '<a target="_blank" href="mailto: haim.kastner@gmail.com" class="ani"><span>Haim Kastner</span></a>'; 
 const main = 'Software Developer - Coding and Development Thoughts Blog, The Content is on the responsibility of the mind reader.';
-const git = '<a target="_blank" href="https://github.com/haimkastner" class="ani"><span>GitHub</span></a>';
-const twitter = '<a target="_blank" href="https://twitter.com/hkastnet" class="ani"><span>Twitter</span></a>';
-const rss = '<a target="_blank" href="/en/feed" class="ani"><span>RSS</span></a>';
 export default {
-  credits: `${user} - ${main} | ${git} | ${twitter} | ${rss} |`,
+  credits: `${user} - ${main}`,
 }
